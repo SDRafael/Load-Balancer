dca0b56f9ceb73b182cc601875c0f6c9537af7f5
sdrafael <j.rafael_jr@outlook.com>
2026-06-08 19:05:07 -0300

    fix: resolvendo dns dinamicamente/add sleep time para avahi daemon

--- app1/00-init-avahi.sh
+++ app1/00-init-avahi.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
 dbus-daemon --system
 avahi-daemon -D
+
+until getent hosts aplicativo2.local
+do
+    sleep 1
+done
+
+until getent hosts aplicativo3.local
+do
+    sleep 1
+done
+
+nginx -g "daemon off;"
